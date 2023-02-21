1cb41d89d7881bb30d120c2371e061f3af8047d4
EC2 Default User <ec2-user@ip-172-31-8-82.ap-northeast-1.compute.internal>
2023-02-21 15:42:32 +0000

    [order_radio_add]

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,16 +1,26 @@
 class Public::OrdersController < ApplicationController
   def new
     @order = Order.new
+    @customer = current_customer
   end
 
   def confirm
+    @order = Order.new(order_params)
+    @address = Address.find(params[:order][:address_id])
+    @order.postal_code = current_customer.postal_code
+    @order.address = current_customer.address
+    @order.name = current_customer.first_name + current_customer.last_name
+    
+    @total_price = @order.inject(0) { |sum, order| sum + order.subtotal }
   end
 
   def complete
   end
   
   def create
-    
+    @order = Order.new
+    @order.save
+    redirect_to orders_confirm_path
   end
 
   def index
@@ -18,4 +28,11 @@ class Public::OrdersController < ApplicationController
 
   def show
   end
+  
+  private
+  
+  def order_params
+     params.require(:order).permit(:method_of_payment, :postal_code, :address, :name)
+  end
+   
 end
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,6 +1,6 @@
 class Address < ApplicationRecord
   belongs_to :customer
     def address_display
-      '〒' + postal_code + ' ' + address + ' ' + name
+      '〒' + postal_code + ' ' + address + ' ' + name 
     end
 end
